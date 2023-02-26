312043855b77d1129551a4ebfb09afe3e2209794
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2023-02-26 18:00:04 +0100

    add login ui with bootstrap 5

---
+++ resources/views/users/login.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="vh-100" style="background-color: #087ef3;">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+
+              <h3 class="mb-5">Sign in</h3>
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group row">
+                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                    <div class="col-md-8">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row my-4">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                    <div class="col-md-8">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+
+                <div>
+                    <button class="btn btn-success w-100 btn-lg btn-block" type="submit">Login</button>
+                </div>
+              </form>
+              <hr class="my-4">
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+@endsection
