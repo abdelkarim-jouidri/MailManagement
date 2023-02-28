c53c22763e5fdb21a901e9f086e248bc0f704783
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2023-02-28 14:03:06 +0100

    add error message in login if login not correct

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -33,7 +33,7 @@ class LoginController extends Controller
         }
 
         return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
+            'email' => 'Les informations d’identification fournies ne correspondent pas à nos dossiers.',
         ]);
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,6 +15,7 @@
                 <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
                     <div class="card z-index-0">
                         <div class="card-header text-center pt-4">
+                           
                             <h5> Création du Compte</h5>
                         </div>
 
