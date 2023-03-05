421a4913dfb1e8857c5932df589b1c5940d265a4
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2023-03-05 23:27:35 +0100

    user or admin can change your password +create rule to check old-password and add some validation in new_pass and confirm_pass

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Rules\CompareOldPassword;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserProfileController extends Controller
 {
@@ -18,30 +21,21 @@ class UserProfileController extends Controller
 
     public function update(Request $request)
     {
-        $attributes = $request->validate([
-            'username' => ['required','max:255', 'min:2'],
-            'firstname' => ['max:100'],
-            'lastname' => ['max:100'],
-            'email' => ['required', 'email', 'max:255',  Rule::unique('users')->ignore(auth()->user()->id),],
-            'address' => ['max:100'],
-            'city' => ['max:100'],
-            'country' => ['max:100'],
-            'postal' => ['max:100'],
-            'about' => ['max:255']
+        // dd($request);
+        $request->validate([
+            // CompareOldPassword => rule to verify password
+            'old_password' => ['required',new CompareOldPassword],
+            'new_password' => ['required','min:5','different:old_password'],
+            'confirm_password' => ['required','min:5','same:new_password'],
+
         ]);
 
-        // auth()->user()->update([
-        //     'username' => $request->get('username'),
-        //     'firstname' => $request->get('firstname'),
-        //     'lastname' => $request->get('lastname'),
-        //     'email' => $request->get('email') ,
-        //     'address' => $request->get('address'),
-        //     'city' => $request->get('city'),
-        //     'country' => $request->get('country'),
-        //     'postal' => $request->get('postal'),
-        //     'about' => $request->get('about')
-        // ]);
-        return back()->with('succes', 'Profile succesfully updated');
+        $user = Auth::user();
+
+       DB::table('users')
+       ->where('id',$user->id)
+       ->update(['password'=>Hash::make($request->new_password)]);
+        return back()->with('succes', 'Password succesfully updated');
     }
 
     // show all users
--- resources/views/pages/user-profile.blade.php
+++ resources/views/pages/user-profile.blade.php
@@ -5,19 +5,13 @@
     <div class="card shadow-lg mx-4 card-profile-bottom">
         <div class="card-body p-3">
             <div class="row gx-4">
-                <div class="col-auto">
-                    <div class="avatar avatar-xl position-relative">
-                        <img src="/img/team-1.jpg" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
-                    </div>
-                </div>
+
                 <div class="col-auto my-auto">
                     <div class="h-100">
                         <h5 class="mb-1">
                             {{ auth()->user()->nom ?? 'Firstname' }} {{ auth()->user()->prenom ?? 'Lastname' }}
                         </h5>
-                        {{-- <p class="mb-0 font-weight-bold text-sm">
-                            Public Relations
-                        </p> --}}
+
                     </div>
                 </div>
 
@@ -35,37 +29,54 @@
                         @csrf
                         <div class="card-header pb-0">
                             <div class="d-flex align-items-center">
-                                <p class="mb-0">Edit Profile</p>
+                                <p class="mb-0">Edit Password</p>
                                 <button type="submit" class="btn btn-primary btn-sm ms-auto">Save</button>
                             </div>
                         </div>
                         <div class="card-body">
-                            <p class="text-uppercase text-sm">User Information</p>
+
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">Username</label>
-                                        <input class="form-control" type="text" name="username" value="{{ old('login', auth()->user()->login) }}">
+                                        <label for="old_password" class="form-control-label">Old Password</label>
+                                        <input class="form-control @error('old_password') is-invalid @enderror" type="password" name="old_password" value="" required autofocus autocomplete="off">
+
+                                        @error('old_password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                     </div>
+
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">Email address</label>
-                                        <input class="form-control" type="email" name="email" value="{{ old('email', auth()->user()->email) }}">
+                                        <label for="new_password" class="form-control-label">New Password</label>
+                                        <input class="form-control @error('new_password') is-invalid @enderror" type="password" name="new_password" value="" required autocomplete="off">
+
+                                        @error('new_password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                     </div>
+
                                 </div>
-                                <div class="col-md-6">
+
+                                <div class="col-md-12">
                                     <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">First name</label>
-                                        <input class="form-control" type="text" name="firstname"  value="{{ old('nom', auth()->user()->nom) }}">
+                                        <label for="confirm_password" class="form-control-label">Confirm New Password</label>
+                                        <input class="form-control @error('confirm_password') is-invalid @enderror" type="password" name="confirm_password" value="" required autocomplete="off">
+                                        @error('confirm_password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                                     </div>
+
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">Last name</label>
-                                        <input class="form-control" type="text" name="lastname" value="{{ old('prenom', auth()->user()->prenom) }}">
-                                    </div>
-                                </div>
+
+
                             </div>
                             <hr class="horizontal dark">
 
